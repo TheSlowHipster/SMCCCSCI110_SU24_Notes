12d4b40377e96bfbe151ead5ef9dc0814c3e41e2
JemmaMac <garnetdroppo@gmail.com>
2024-06-12 11:54:35 -0400

    vault backup: 2024-06-12 11:54:35

---
+++ LightspeedCalculator.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class LightspeedCalculator {
+    public static void main(String[] args) {
+        final double LIGHT_SPEED_MPH = 671000001.0;
+        System.out.println("How many miles is it to the planet?");
+        // Open the terminal input as a scanner
+        Scanner scnr = new Scanner(System.in);
+        double distance = scnr.nextDouble();
+        System.out.println("Your planet would take " + distance / LIGHT_SPEED_MPH + " hours to travel to.");
+    }
+}
