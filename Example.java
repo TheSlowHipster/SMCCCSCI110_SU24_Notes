2970157c5df5adfe5871df178d81dfeb7fdbc44b
JemmaMac <garnetdroppo@gmail.com>
2024-06-26 18:04:12 -0400

    vault backup: 2024-06-26 18:04:12

--- Example.java
+++ Example.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 class Example {
 
+    public run(){
+        Test val = works;
+    }
+
     public static void main(String[] args) {
-        int x = 10;
-        int y = 5;
-        int z = 9;
-        int w = 3;
-        System.out.println(((0.5 * x - y) < 0) && ((z / w) == 3));
-        for (int i = 0; true; i++) {
-            System.out.println(i);
+        enum Test {
+            works, not
         }
+        ;
     }
 }
