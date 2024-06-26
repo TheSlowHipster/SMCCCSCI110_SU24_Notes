7d2f44871bdfc52f9ebf0d8ce3b05816dccc817f
JemmaMac <garnetdroppo@gmail.com>
2024-06-26 14:37:19 -0400

    vault backup: 2024-06-26 14:37:19

--- Example.java
+++ Example.java
@@ -3,32 +3,13 @@ import java.util.Scanner;
 class Example {
 
     public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
-        System.out.print("How many pets do you have? ");
-        int numPets = scnr.nextInt();
-        if ((numPets < 4) && (numPets >= 0)) {
-            System.out.println("You have a reasonable number of pets.");
-        } else if ((numPets <= 6) || (numPets == 100)) {
-            System.out.println("Wow, you have a lot of pets!");
-        } else if (!(numPets > 90)) {
-            System.out.println("You have so many pets how do you take care of them all?");
-        } else {
-            System.out.println("That's an interesting number of pets.");
+        int x = 10;
+        int y = 5;
+        int z = 9;
+        int w = 3;
+        System.out.println(((0.5 * x - y) < 0) && ((z / w) == 3));
+        for (int i = 0; true; i++) {
+            System.out.println(i);
         }
-        System.out.println("After the if block");
-        for (int i = 0; i < 11; i = i + 1) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        String input;
-        do {
-            System.out.println("Type 'jem' to continue");
-            input = scnr.next();
-        } while (!input.equals("jem"));
-        int[] ints = { 1, 2, 3, 4, 5 };
-        for (int i : ints) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
     }
 }
