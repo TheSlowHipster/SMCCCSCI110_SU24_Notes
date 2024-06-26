f6c5a5b948ccf5ae9c18789150a9b9692a595955
JemmaMac <garnetdroppo@gmail.com>
2024-06-26 19:24:46 -0400

    vault backup: 2024-06-26 19:24:46

--- Example.java
+++ Example.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
 
 class Example {
-
-    public run(){
-        Test val = works;
+    public enum Difficulty {
+        EASY, BALANCED, HARD, UNFAIR
     }
 
     public static void main(String[] args) {
-        enum Test {
-            works, not
-        }
-        ;
+        Difficulty choice = getUserDifficultyChoice();
+        switch (choice):
+            case EASY:
+                setPlayerHealth(200);
+                break;
+            case BALANCED:
+                setPlayerHealth(100);
+                break;
+            case HARD:
+                setPlayerHealth(50);
+                break;
+            case UNFAIR:
+                setPlayerHealth(25);
+                break;
     }
 }
