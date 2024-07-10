abf18dc5003c73843c49619736557a4fbd9f73a0
JemmaMac <garnetdroppo@gmail.com>
2024-07-10 14:01:51 -0400

    vault backup: 2024-07-10 14:01:51

--- Example.java
+++ Example.java
@@ -1,24 +1,11 @@
 import java.util.Scanner;
 
 class Example {
-    public enum Difficulty {
-        EASY, BALANCED, HARD, UNFAIR
-    }
-
     public static void main(String[] args) {
-        Difficulty choice = getUserDifficultyChoice();
-        switch (choice):
-            case EASY:
-                setPlayerHealth(200);
-                break;
-            case BALANCED:
-                setPlayerHealth(100);
-                break;
-            case HARD:
-                setPlayerHealth(50);
-                break;
-            case UNFAIR:
-                setPlayerHealth(25);
-                break;
+        int x = 10;
+        int y = 5;
+        int z = 9;
+        int w = 3;
+        System.out.println(((0.5 * x - y) < 0) || ((z / w) == 3));
     }
 }
