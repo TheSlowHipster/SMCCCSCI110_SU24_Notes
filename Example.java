bbf8739ed5a922ad9b0f90671fe4840206435618
JemmaMac <garnetdroppo@gmail.com>
2024-07-11 12:11:49 -0400

    vault backup: 2024-07-11 12:11:49

--- Example.java
+++ Example.java
@@ -1,7 +1,25 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Example {
-    public static void main(String[] args) {
+    // public - anyone can use this
+    // static - shared across all instances (created by `new Example()`)
+    // final - does not change
+    public static final int SET_SIZE = 10;
 
+    // private - Only this class
+
+    // protected - Only this package
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        Test testOne = new Test();
+        testOne.var = 5;
+        Test testTwo = new Test();
+
+        System.out.println(testOne);
+        System.out.println(testTwo);
+        System.out.println("\n");
     }
 }
--- LightspeedCalculator.java
+++ LightspeedCalculator.java
@@ -1,8 +1,10 @@
 import java.util.*;
 
 class LightspeedCalculator {
+    public static double LIGHT_SPEED_MPH = 671000001.0;
+
     public static void main(String[] args) {
-        final double LIGHT_SPEED_MPH = 671000001.0;
+
         System.out.println("How many miles is it to the planet?");
         // Open the terminal input as a scanner
         Scanner scnr = new Scanner(System.in);
